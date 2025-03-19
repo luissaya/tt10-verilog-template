37578c97d8d46f3db2da629d976c9aed20a9f273
luisASUSTUF <luisASUSTUFF@ubuntu.com>
2025-03-19 11:33:05 -0300

    update: solve ports names

--- src/project.v
+++ src/project.v
@@ -1,19 +1,41 @@
-module tt_um_sync_fifo_luisaya( clk, rst_n, wr_ena, rd_ena, wr_data, rd_data, full, empty);
+`default_nettype none
+module tt_um_sync_fifo_luisaya( 
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
 
-// Defining parameters
-parameter FIFO_WIDTH = 4,
-          FIFO_DEPTH = 8,
-          ADDR_WIDTH = 3;
+  // Defining parameters
+  parameter FIFO_WIDTH = 4,
+            FIFO_DEPTH = 8,
+            ADDR_WIDTH = 3;
 
-// Definig inputs ports
-input                   clk, rst_n;
-input [FIFO_WIDTH-1:0]  wr_data;
-input                   wr_ena, rd_ena;
+  wire [FIFO_WIDTH-1:0]  wr_data;
+  wire wr_ena, rd_ena;
+  wire [FIFO_WIDTH-1:0] rd_data;
+  reg  full;
+  reg  empty;
 
-// Defining outputs ports 
-output [FIFO_WIDTH-1:0] rd_data;
-output reg              full;
-output reg              empty;
+  // Definig inputs ports
+  assign ui_in[3:0] = wr_data;
+  assign ui_in[5:4] = {wr_ena, rd_ena};
+
+  // Defining outputs ports 
+  assign uo_out[3:0] = rd_data;
+  assign uo_out[5:4] = {full, empty};
+
+  // All output pins must be assigned. If not used, assign to 0.
+  assign uo_out[7:6]  = 0;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uio_out = 0;
+  assign uio_oe  = 0;
+
+  // List all unused inputs to prevent warnings
+  wire _unused = &{ena, ui_in[7:6], uio_in, 1'b0};
 
 // Defining internal signals
 reg [ADDR_WIDTH:0]  wr_ptr;
